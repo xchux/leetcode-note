1fc4545e7a63860d04afb9a761b719d278c02f51
Simon Chu <simonchu199510@gmail.com>
2025-03-27 14:10:47 +0800

    :sparkles: (solution): Problem 42. Trapping Rain Water
    Add Solution of 42. Trapping Rain Water

---
+++ solutions/42. Trapping Rain Water/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = height[left], rightMax = height[right];
+        int water = 0;
+
+        while (left < right) {
+            if (leftMax < rightMax) {
+                left++;
+                leftMax = Math.max(leftMax, height[left]);
+                water += leftMax - height[left];
+            } else {
+                right--;
+                rightMax = Math.max(rightMax, height[right]);
+                water += rightMax - height[right];
+            }
+        }
+        return water;
+    }
+}
